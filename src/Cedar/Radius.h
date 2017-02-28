9c98cbb3b6adaa0c8d6538922c8a146f36a7500f
Mountain Fury <garlicnarwal@gmail.com>
2017-03-01 09:01:28 +1030

    Updated the RADIUS timeout
    Updated the RADIUS timeout mechanism, this should be configurable
    within the application, but updated the hardcoded value for now.

--- src/Cedar/Radius.h
+++ src/Cedar/Radius.h
@@ -1,32 +1,32 @@
 // SoftEther VPN Source Code
 // Cedar Communication Module
-// 
+//
 // SoftEther VPN Server, Client and Bridge are free software under GPLv2.
-// 
+//
 // Copyright (c) 2012-2016 Daiyuu Nobori.
 // Copyright (c) 2012-2016 SoftEther VPN Project, University of Tsukuba, Japan.
 // Copyright (c) 2012-2016 SoftEther Corporation.
-// 
+//
 // All Rights Reserved.
-// 
+//
 // http://www.softether.org/
-// 
+//
 // Author: Daiyuu Nobori
 // Comments: Tetsuo Sugiyama, Ph.D.
-// 
+//
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // version 2 as published by the Free Software Foundation.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License version 2
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -34,11 +34,11 @@
 // CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// 
+//
 // THE LICENSE AGREEMENT IS ATTACHED ON THE SOURCE-CODE PACKAGE
 // AS "LICENSE.TXT" FILE. READ THE TEXT FILE IN ADVANCE TO USE THE SOFTWARE.
-// 
-// 
+//
+//
 // THIS SOFTWARE IS DEVELOPED IN JAPAN, AND DISTRIBUTED FROM JAPAN,
 // UNDER JAPANESE LAWS. YOU MUST AGREE IN ADVANCE TO USE, COPY, MODIFY,
 // MERGE, PUBLISH, DISTRIBUTE, SUBLICENSE, AND/OR SELL COPIES OF THIS
@@ -53,7 +53,7 @@
 // JAPAN. YOU MUST WAIVE ALL DEFENSES OF LACK OF PERSONAL JURISDICTION
 // AND FORUM NON CONVENIENS. PROCESS MAY BE SERVED ON EITHER PARTY IN
 // THE MANNER AUTHORIZED BY APPLICABLE LAW OR COURT RULE.
-// 
+//
 // USE ONLY IN JAPAN. DO NOT USE THIS SOFTWARE IN ANOTHER COUNTRY UNLESS
 // YOU HAVE A CONFIRMATION THAT THIS SOFTWARE DOES NOT VIOLATE ANY
 // CRIMINAL LAWS OR CIVIL RIGHTS IN THAT PARTICULAR COUNTRY. USING THIS
@@ -73,37 +73,37 @@
 // RECOVER OR COMPENSATE SUCH DAMAGES, CRIMINAL OR CIVIL
 // RESPONSIBILITIES. NOTE THAT THIS LINE IS NOT LICENSE RESTRICTION BUT
 // JUST A STATEMENT FOR WARNING AND DISCLAIMER.
-// 
-// 
+//
+//
 // SOURCE CODE CONTRIBUTION
 // ------------------------
-// 
+//
 // Your contribution to SoftEther VPN Project is much appreciated.
 // Please send patches to us through GitHub.
 // Read the SoftEther VPN Patch Acceptance Policy in advance:
 // http://www.softether.org/5-download/src/9.patch
-// 
-// 
+//
+//
 // DEAR SECURITY EXPERTS
 // ---------------------
-// 
+//
 // If you find a bug or a security vulnerability please kindly inform us
 // about the problem immediately so that we can fix the security problem
 // to protect a lot of users around the world as soon as possible.
-// 
+//
 // Our e-mail address for security reports is:
 // softether-vpn-security [at] softether.org
-// 
+//
 // Please note that the above e-mail address is not a technical support
 // inquiry address. If you need technical assistance, please visit
 // http://www.softether.org/ and ask your question on the users forum.
-// 
+//
 // Thank you for your cooperation.
-// 
-// 
+//
+//
 // NO MEMORY OR RESOURCE LEAKS
 // ---------------------------
-// 
+//
 // The memory-leaks and resource-leaks verification under the stress
 // test has been passed before release this source code.
 
@@ -116,7 +116,7 @@
 
 #define	RADIUS_DEFAULT_PORT		1812			// The default port number
 #define	RADIUS_RETRY_INTERVAL	500				// Retransmission interval
-#define	RADIUS_RETRY_TIMEOUT	(10 * 1000)		// Time-out period
+#define	RADIUS_RETRY_TIMEOUT	(30 * 1000)		// Time-out period
 #define	RADIUS_INITIAL_EAP_TIMEOUT	1600		// Initial timeout for EAP
 
 
